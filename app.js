163f3bb10ac1d7dc7d5c76aefe2dca4ade14f395
Cesar Jesus Alberto Alvan Huarcaya <jesusalvan2010@gmail.com>
2023-10-08 14:18:22 -0500

    feat: setup meta provider adding welcome step :sparkles:

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
-const { createBot, createProvider, createFlow } = require('@bot-whatsapp/bot');
-
 const { cache } = require('./config/cache');
-const { MONGO_DB_URI, MONGO_DB_NAME } = require('./config/db/Env.db');
-
-const QRPortalWeb = require('@bot-whatsapp/portal');
-const BaileysProvider = require('@bot-whatsapp/provider/baileys');
-const MongoAdapter = require('@bot-whatsapp/database/mongo');
-
 const { client } = require('./config/db/Singleton.db');
-const { welcomeStepFlow } = require('./flows/steps/welcome-steps/welcome.step');
+
+const { startup } = require('./startups/startup-meta');
 
 const main = async () => {
   try {
-    const adapterDB = new MongoAdapter({
-      dbUri: MONGO_DB_URI,
-      dbName: MONGO_DB_NAME,
-    });
-    const adapterFlow = createFlow([welcomeStepFlow]);
-    const adapterProvider = createProvider(BaileysProvider);
-
-    createBot({
-      flow: adapterFlow,
-      provider: adapterProvider,
-      database: adapterDB,
-    });
-
-    QRPortalWeb();
-
+    startup();
     await client();
     cache();
   } catch (error) {
+    console.log('test????', error);
     setTimeout(() => {
       main();
     }, 10 * 1000);
